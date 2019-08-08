64b819bef5e711ca0361c1234f4d9d78f3c1340c
dunossauro <mendesxeduardo@gmail.com>
2019-08-08 16:45:04 -0300

    setup update

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup
 
 setup(
     name='report-portal-hook-plugin',
-    version='0.0.1',
+    version='0.0.2',
     description='behave report portal hook plugin',
     url='https://github.com/dunossauro/report-portal-hook-plugin',
     author='Eduardo Mendes',
     author_email='mendesxeduardo@gmail.com',
     license='MIT',
     packages=['report_portal'],
+    keywords=['testing', 'reporting', 'reportportal', 'hook_plug', 'behave'],
+    install_requires=['reportportal-client'],
+    dependency_links=[
+        'https://github.com/dunossauro/hook_plug/master#egg=hook_plug'
+    ],
     zip_safe=False,
 )
